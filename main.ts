27153fec68c30bf9720921e502ff153bd8a724a7
Johan Gorter <johan-gorter@users.noreply.github.com>
2022-12-28 21:33:42 +0100

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ namespace macarena {
           . # # # .
           . . # . .
           . # . # .
-          `, 0)
+          `, 500)
     }
 
     //% block
@@ -22,6 +22,62 @@ namespace macarena {
           . # # . .
           . # # # .
           . . # . .
+          . # . # .
+          `, 0)
+        music.playMelody("G:3 - G:1 - G:1 -", 100)
+    }
+
+    //% block
+    //% block.loc.nl="rechter arm vooruit"
+    //% weight=980
+    export function rightArmForward() {
+        basic.showLeds(`
+          . # . # .
+          . # # # .
+          . # # # .
+          . . # . .
+          . # . # .
+          `, 0)
+        music.playMelody("G:3 - G:1 - G:1 -", 100)
+    }
+
+    //% block
+    //% block.loc.nl="linker hand draaien"
+    //% weight=970
+    export function turnLeftHand() {
+        basic.showLeds(`
+          # # . # .
+          . # # # .
+          . # # # .
+          . . # . .
+          . # . # .
+          `, 0)
+        music.playMelody("G:1 - G:1 - G:1 - G:1 -", 100)
+    }
+
+    //% block
+    //% block.loc.nl="rechter hand draaien"
+    //% weight=960
+    export function turnRightHand() {
+        basic.showLeds(`
+          # # . # .
+          . # # # .
+          . # # # .
+          . . # . .
+          . # . # .
+          `, 0)
+        music.playMelody("G:1 - B:1 - D:1 - D:1 -", 100)
+    }
+
+    //% block
+    //% block.loc.nl="linker hand naar rechter heup"
+    //% weight=950
+    export function leftHandToRightHip() {
+        basic.showLeds(`
+          . . . # .
+          . . # # .
+          # # # # .
+          . # # . .
           . # . # .
           `, 0)
         music.playMelody("G:3 - G:1 - G:1 -", 100)
