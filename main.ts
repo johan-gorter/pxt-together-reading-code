559a8d63453e7274b6ca2febeb9aebb6327b583e
Johan Gorter <johan-gorter@users.noreply.github.com>
2023-01-09 22:50:45 +0100

    scavenger hunt added

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 namespace macarena {
 
-    let tempo: number;
+    export let tempo: number = 80;
 
     //% block="stand up"
     //% block.loc.nl-NL="sta op"
     //% weight=1000
     export function standUp() {
+        tempo = 80;
         basic.showLeds(`
           . . . . .
           . . # . .
@@ -228,5 +229,99 @@ namespace macarena {
     //% weight=400
     export function applause() {
         music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 380, 380, 121, 14, 1500, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    }
+}
+
+//% color=190 weight=100 icon="\uf1ec" block="Scavenger hunt"
+//% block.loc.nl-NL="Speurtocht"
+namespace scavengerHunt {
+    export let direction: number = 0;
+
+    function showDirection() {
+        basic.showArrow(direction, 300);
+    }
+
+    //% block
+    //% block.loc.nl-NL="draai naar links"
+    export function turnLeft() {
+        direction--;
+        if (direction < 0) {
+            direction = 7;
+        }
+        showDirection();
+        direction--;
+        showDirection();
+    }
+
+    //% block
+    //% block.loc.nl-NL="draai naar rechts"
+    export function turnRight() {
+        direction++;
+        if (direction > 7) {
+            direction = 0;
+        }
+        showDirection();
+        direction++;
+        showDirection();
+    }
+
+    //% block
+    //% block.loc.nl-NL="draai om"
+    export function turnAround() {
+        turnRight();
+        turnRight();
+    }
+
+    //% block
+    //% block.loc.nl-NL="loop vooruit"
+    export function walkForward() {
+    }
+
+    //% block
+    //% block.loc.nl-NL="loop $tileCount vooruit"
+    //% tileCount.loc.nl-NL="aantal tegels"
+    export function walkTilesForward(tileCount: number) {
+    }
+
+    //% block
+    //% block.loc.nl-NL="loop $tileCount achteruit"
+    //% tileCount.loc.nl-NL="aantal tegels"
+    export function walkTilesBackwards(tileCount: number) {
+    }
+
+    //% block
+    //% block.loc.nl-NL="was $description af"
+    //% description.loc.nl-NL="omschrijving"
+    export function wash(description: string) {
+    }
+
+    //% block
+    //% block.loc.nl-NL="droog $description af"
+    //% description.loc.nl-NL="omschrijving"
+    export function dry(description: string) {
+    }
+
+    //% block
+    //% block.loc.nl-NL="maak een"
+    //% description.loc.nl-NL="omschrijving"
+    export function makeA(description: string) {
+    }
+
+    //% block
+    //% block.loc.nl-NL="volg"
+    //% description.loc.nl-NL="omschrijving"
+    export function follow(description: string) {
+    }
+
+    //% block
+    //% block.loc.nl-NL="volg mij"
+    export function followMe() {
+    }
+
+    //% block
+    //% block.loc.nl-NL="ik"
+    //% description.loc.nl-NL="omschrijving"
+    export function iAm(description: string): boolean {
+        return false;
     }
 }
